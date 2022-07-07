2d5cd3fedcc7dc8ad9ae8a8a81b6c9be59ae0e49
Taewan Kim <paladyn12@naver.com>
2022-07-07 17:03:27 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a220707kotlinpractice/SubActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a220707kotlinpractice
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SubActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
