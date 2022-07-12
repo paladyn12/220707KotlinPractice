c5188d9752a8cc0bc643c83d5e8d6e74caddbf22
Taewan Kim <paladyn12@naver.com>
2022-07-12 16:43:09 +0900

    220711 kotlinPractice

---
+++ app/src/main/java/com/example/a220707kotlinpractice/JavaActivity.java
@@ -0,0 +1,14 @@
+package com.example.a220707kotlinpractice;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JavaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_java);
+    }
+}
